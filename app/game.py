0547232d1ee4a47754490bee3f395c573f2bd78c
mobisare <m.luc57@outlook.fr>
2021-07-11 17:03:11 +0200

    Déplacements du Joueur Ajout des collisions en cours

--- app/game.py
+++ app/game.py
@@ -25,23 +25,52 @@ class Game:
         tmx_data = pytmx.util_pygame.load_pygame(map)
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 2
 
         # Génération du joueur
-        self.player = player.Player()
+        player_position = tmx_data.get_object_by_name("player")
+        self.player = player.Player(player_position.x, player_position.y)
 
         # dessiner le groupe de calques
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
         self.group.add(self.player)
 
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+            self.player.change_animation('up')
+
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+            self.player.change_animation('down')
+
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+            self.player.change_animation('left')
+
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+            self.player.change_animation('right')
+
     def _run(self):
+
+        clock = pygame.time.Clock()
+
         # Boucle du jeu
         running = True
         while running:
+            self.handle_input()
+            self.group.update()
+            self.group.center(self.player.rect)
             self.group.draw(self.screen)
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            clock.tick(60)
         pygame.quit()
 
     def get_screen(self):
--- app/player.py
+++ app/player.py
@@ -5,13 +5,41 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
 
         player = os.path.join("ressources", "player", "player.png")
         self.sprite_sheet = pygame.image.load(player)
         self.image = self.get_image(0, 0)
+        self.image.set_colorkey([0, 0, 0, 0])
         self.rect = self.image.get_rect()
+        self.position = [x, y]
+        self.images = {
+            'down': self.get_image(0, 0),
+            'left': self.get_image(0, 32),
+            'right': self.get_image(0, 64),
+            'up': self.get_image(0, 96)
+        }
+        self.speed = 3
+
+    def change_animation(self, name):
+        self.image = self.images[name]
+        self.image.set_colorkey((0, 0, 0))
+
+    def update(self):
+        self.rect.topleft = self.position
+
+    def move_right(self):
+        self.position[0] += self.speed
+
+    def move_left(self):
+        self.position[0] -= self.speed
+
+    def move_down(self):
+        self.position[1] += self.speed
+
+    def move_up(self):
+        self.position[1] -= self.speed
 
     def get_image(self, x, y):
         image = pygame.Surface([32, 32])
